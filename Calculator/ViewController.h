d6af9aec9252e792944e516cb88181b0c4ac1b71
shesl-meow <shesonglin@bytedance.com>
2019-09-16 14:44:47 +0800

    Initial Commit

---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by shesonglin on 2019/9/16.
+//  Copyright © 2019 shesonglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Calculator/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Calculator
+//
+//  Created by shesonglin on 2019/9/16.
+//  Copyright © 2019 shesonglin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
